c28e8491a3836abf807462fed98cbe32f2d5f472
DAVIDE CARIA s309305 <davidejan23@gmail.com>
2023-12-18 16:08:03 +0100

    iptable basic configuration added

--- lab/hardening/main-hardening.sh
+++ lab/hardening/main-hardening.sh
@@ -3,7 +3,9 @@
 sudo su
 
 
-####### Creating a backup policy and automatic schedule
+echo "#########################################################"
+echo "#### Creating a backup policy and automatic schedule ####"
+echo "#########################################################"
 
 mkdir /backups/
 
@@ -19,3 +21,36 @@ chmod +x backup_script.sh
 
 # Schedule backup_script.sh to run daily
 echo "0 0 * * * /path/to/backup_script.sh" | crontab -
+
+echo "#########################################################"
+echo "###### Configuring IPTable allowlist and blocklist ######"
+echo "#########################################################"
+
+# Flush existing rules and set the default policies to DROP
+iptables -F
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow incoming SSH 
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow incoming HTTP
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# Allow incoming HTTPS
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allow loopback interface
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow established and related connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Log and drop other incoming connections
+iptables -A INPUT -j LOG --log-prefix "iptables: " --log-level 7
+iptables -A INPUT -j DROP
+
+# Save the rules to persist across reboots
+iptables-save > /etc/iptables/rules.v4
