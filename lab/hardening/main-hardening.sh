f49cc42a72002136d8c99534ffb48c76582e4847
DAVIDE CARIA s309305 <davidejan23@gmail.com>
2023-12-18 15:38:02 +0100

    backup strategy and retention

--- lab/hardening/main-hardening.sh
+++ lab/hardening/main-hardening.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 sudo su
+
+
+####### Creating a backup policy and automatic schedule
+
+mkdir /backups/
+
+echo -e "#!/bin/bash\n\n\
+SOURCE_DIR=\"~/\"\n\
+DEST_DIR=\"/backups/\"\n\
+TIMESTAMP=\$(date +%Y%m%d%H%M%S)\n\n\
+rsync -a --delete \$SOURCE_DIR \$DEST_DIR/backup_\$TIMESTAMP\n\
+tar czf \$DEST_DIR/snapshot_\$TIMESTAMP.tar.gz -C \$DEST_DIR backup_\$TIMESTAMP\n\
+find \$DEST_DIR -type d -name \"backup_*\" -mtime +3 -exec rm -r {} \;" > backup_script.sh
+
+chmod +x backup_script.sh
+
+# Schedule backup_script.sh to run daily
+echo "0 0 * * * /path/to/backup_script.sh" | crontab -
